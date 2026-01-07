ccc3f9678a8209f0a98aa7f1c699dd48c50a66c0
MODUGGAGI <kiroro0814@naver.com>
2026-01-07 19:13:24 +0900

    feat(view): 출석 확인 후 상태 출력
    출석 확인 후 그 상태가 어떻게 되었는지 출력하는 메소드를 추가했습니다.

--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -1,16 +1,18 @@
 package attendance.view;
 
 import attendance.domain.AttendanceTime;
+import attendance.dto.AttendResultDTO;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 public class OutputView {
 
     private static final String GREETING = "오늘은 %d월 %d일 %s요일입니다. 기능을 선택해 주세요.";
+    private static final String ATTEND_RESULT = "%d월 %d일 %s요일 %d:%d (%s)";
 
     public void printGreeting(LocalDateTime now) {
         System.out.printf(GREETING + "\n",
-                now.getMonth().getValue(),
+                now.getMonthValue(),
                 now.getDayOfMonth(),
                 parseDayOfWeek(now.getDayOfWeek())
         );
@@ -23,5 +25,16 @@ public class OutputView {
     public void printError(String errorMessage) {
         System.out.println(errorMessage);
         System.out.println();
+    }
+
+    public void printAttendResult(AttendResultDTO result) {
+        System.out.println();
+        System.out.printf(ATTEND_RESULT + "\n",
+                result.now().getMonthValue(),
+                result.now().getDayOfMonth(),
+                parseDayOfWeek(result.now().getDayOfWeek()),
+                result.now().getHour(),
+                result.now().getMinute(),
+                result.status().getStatus());
     }
 }
