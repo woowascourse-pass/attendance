a1aed9d0fd93b6ef93eae101cfa55661fe2648e4
MODUGGAGI <kiroro0814@naver.com>
2026-01-07 17:06:51 +0900

    feat(domain) : 크루 생성 구현
    크루의 정보를 담기 위한 Crew 클래스, 그리고 그런 Crew들을 모아서 관리하기 위한 AttendanceBook 클래스를 구현했고 출석 날짜를 관리하기 위해 ENUM으로 출석 시간과 상태를 생성해서 관리하게 했습니다.
    그리고 CSV 파일에서 읽어온 데이터를 바탕으로 Crew에 집어넣어 생성했습니다.

---
+++ src/main/java/attendance/domain/Status.java
@@ -0,0 +1,28 @@
+package attendance.domain;
+
+import java.time.LocalTime;
+
+public enum Status {
+    ATTENDANCE("출석"),
+    LATE("지각"),
+    ABSENT("결석");
+
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public static Status getStatus(int timeDiff) {
+
+        if (30 < timeDiff) {
+            return ABSENT;
+        }
+
+        if (5 < timeDiff) {
+            return LATE;
+        }
+
+        return ATTENDANCE;
+    }
+}
