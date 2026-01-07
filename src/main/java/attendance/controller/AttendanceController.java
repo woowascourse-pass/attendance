48761451f863795c89c26b053cd615ef1ef11414
MODUGGAGI <kiroro0814@naver.com>
2026-01-07 19:11:11 +0900

    feat(controller) : 출석 확인 기능 구현
    출석 확인을 위해 사용자에게 값을 받아와 도메인과 뷰에게 각각 처리 요청 및 결과 전달을 구현하였습니다.

--- src/main/java/attendance/controller/AttendanceController.java
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -1,14 +1,19 @@
 package attendance.controller;
 
 import attendance.domain.AttendanceBook;
+import attendance.domain.AttendanceTime;
+import attendance.dto.AttendResultDTO;
 import attendance.util.InputFileReader;
 import attendance.util.InputValidator;
 import attendance.util.Parser;
 import attendance.view.InputView;
 import attendance.view.OutputView;
 import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
 
 public class AttendanceController {
+
+    private static final String END = "Q";
 
     private final InputView inputView;
     private final OutputView outputView;
@@ -22,12 +27,14 @@ public class AttendanceController {
 
     public void start() {
         String select;
-        while (!((select = readFunction()).equals("Q"))) {
+        LocalDateTime now;
+        boolean shouldContinue = true;
+        while (shouldContinue && !((select = readFunction(now = DateTimes.now())).equals(END))) {
 
             int number = Parser.parseSelect(select);
 
             if (number == 1) {
-
+                shouldContinue = attendanceCheck(now);
             }
 
             if (number == 2) {
@@ -44,15 +51,36 @@ public class AttendanceController {
         }
     }
 
-    private String readFunction() {
-        while (true) {
-            try {
-                outputView.printGreeting(DateTimes.now());
-                String selectedFunction = inputView.readFunction();
-                return InputValidator.validateSelectedFunction(selectedFunction);
-            } catch (IllegalArgumentException e) {
-                outputView.printError(e.getMessage());
-            }
+    private boolean attendanceCheck(LocalDateTime now) {
+        try {
+            // 주말 체크
+            AttendanceTime.checkWeekend(now);
+            String name = inputView.readName();
+            // 이름 체크
+            String crewName = attendanceBook.validateCrewName(name);
+            // 이미 출석 했는지 체크
+            attendanceBook.validateAttend(crewName, now);
+            // 등교 시간 입력
+            String attendTime = inputView.readAttendTime();
+            LocalDateTime parsedAttendTime = Parser.parseTime(now, attendTime);
+            // 출석 체크
+            AttendResultDTO result = attendanceBook.attend(crewName, parsedAttendTime);
+            outputView.printAttendResult(result);
+            return true;
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            return false;
+        }
+    }
+
+    private String readFunction(LocalDateTime now) {
+        try {
+            outputView.printGreeting(now);
+            String selectedFunction = inputView.readFunction();
+            return InputValidator.validateSelectedFunction(selectedFunction);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            return END;
         }
     }
 }
--- src/main/java/attendance/util/Parser.java
+++ src/main/java/attendance/util/Parser.java
@@ -1,6 +1,7 @@
 package attendance.util;
 
 import attendance.message.ErrorMessage;
+import java.time.LocalDateTime;
 
 public class Parser {
     public static int parseSelect(String select) {
@@ -8,6 +9,22 @@ public class Parser {
             int parseInput = Integer.parseInt(select);
             return InputValidator.validateParsedInput(parseInput);
         } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
+        }
+    }
+
+    public static LocalDateTime parseTime(LocalDateTime now, String attendTime) {
+        try {
+            String[] inputTime = attendTime.split(":");
+
+            return LocalDateTime.of(
+                    now.getYear(),
+                    now.getMonthValue(),
+                    now.getDayOfMonth(),
+                    Integer.parseInt(inputTime[0]),
+                    Integer.parseInt(inputTime[1]),
+                    0);
+        } catch (Exception e) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
         }
     }
