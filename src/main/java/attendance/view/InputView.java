20876689b2ba1d966040c4385767e16b731cd709
MODUGGAGI <kiroro0814@naver.com>
2026-01-07 19:13:46 +0900

    feat(view): 출석 확인을 위한 값 입력 메소드 구현
    출석 확인을 위한 값을 입력받는 메소드를 구현했습니다.

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -10,9 +10,23 @@ public class InputView {
             + "3. 크루별 출석 기록 확인\n"
             + "4. 제적 위험자 확인\n"
             + "Q. 종료";
+    private static final String GET_NAME = "닉네임을 입력해 주세요.";
+    private static final String GET_ATTEND_TIME = "등교 시간을 입력해 주세요.";
 
     public String readFunction() {
         System.out.println(FUNCTION);
+        return Console.readLine();
+    }
+
+    public String readName() {
+        System.out.println();
+        System.out.println(GET_NAME);
+        return Console.readLine();
+    }
+
+    public String readAttendTime() {
+        System.out.println();
+        System.out.println(GET_ATTEND_TIME);
         return Console.readLine();
     }
 }
