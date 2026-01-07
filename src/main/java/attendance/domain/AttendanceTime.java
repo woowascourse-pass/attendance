8f0e2502980762b5d97612610d2fb5720cd68910
MODUGGAGI <kiroro0814@naver.com>
2026-01-07 17:31:36 +0900

    feat(domain) : 각 요일 및 필드로 한글 요일 추가
    모든 요일들을 명확하게 추가했고 해당 요일들의 한글 이름을 필드로 추가해서 정적 메소드로 특정 요일의 한글이름이 무엇인지 반환하는 기능을 추가했습니다.

--- src/main/java/attendance/domain/AttendanceTime.java
+++ src/main/java/attendance/domain/AttendanceTime.java
@@ -1,20 +1,31 @@
 package attendance.domain;
 
+import attendance.message.ErrorMessage;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 public enum AttendanceTime {
-    MONDAY(LocalTime.of(13, 0, 0), LocalTime.of(18, 0, 0)),
-    OTHER(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0)),
+    MONDAY(LocalTime.of(13, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.MONDAY, "월"),
+    TUESDAY(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.TUESDAY, "화"),
+    WEDNESDAY(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.WEDNESDAY, "수"),
+    THURSDAY(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.THURSDAY, "목"),
+    FRIDAY(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.FRIDAY, "금"),
+    SATURDAY(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.SATURDAY, "토"),
+    SUNDAY(LocalTime.of(10, 0, 0), LocalTime.of(18, 0, 0), DayOfWeek.SUNDAY, "일"),
     ;
 
     private final LocalTime startTime;
     private final LocalTime endTime;
+    private final DayOfWeek dayOfWeek;
+    private final String koreanDayOfWeek;
 
-    AttendanceTime(LocalTime startTime, LocalTime endTime) {
+    AttendanceTime(LocalTime startTime, LocalTime endTime, DayOfWeek dayOfWeek, String koreanDayOfWeek) {
         this.startTime = startTime;
         this.endTime = endTime;
+        this.dayOfWeek = dayOfWeek;
+        this.koreanDayOfWeek = koreanDayOfWeek;
     }
 
     public static Status checkStatus(DayOfWeek dayOfWeek, LocalTime attendTime) {
@@ -22,7 +33,16 @@ public enum AttendanceTime {
             return getStatusByDayOfWeek(MONDAY.startTime, attendTime);
         }
 
-        return getStatusByDayOfWeek(OTHER.startTime, attendTime);
+        return getStatusByDayOfWeek(TUESDAY.startTime, attendTime);
+    }
+
+    public static String getKoreanDayOfWeek(DayOfWeek dayOfWeek) {
+        AttendanceTime found = Arrays.stream(values())
+                .filter(attendanceTime -> attendanceTime.dayOfWeek == dayOfWeek)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_DAY_OF_WEEK.getMessage()));
+
+        return found.koreanDayOfWeek;
     }
 
     private static Status getStatusByDayOfWeek(LocalTime startTime, LocalTime attendTime) {
